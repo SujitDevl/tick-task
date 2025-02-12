9ac70c230d1a371ade54973e682c20231c857ed3
SujitDevl <pandasujit687@gmail.com>
2025-02-13 00:28:35 +0530

    adding jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import "./style.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <h1>Tick Task</h1>
       <h3>Effortlessly add, manage, and track tasks your productivity</h3>
 
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">Add Task:</label>
-          <input type="text" id="item" placeholder="Enter your task..." />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+            placeholder="Enter your task..."
+          />
         </div>
+        <button className="btn">Add</button>
       </form>
       <h1 className="header">Todos:</h1>
       <ul className="list">
